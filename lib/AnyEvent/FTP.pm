c8ffd73e2c67d2b429fa2ced6bd3ebbe2912f929
Graham Ollis <perl@wdlabs.com>
2013-01-23 23:02:12 -0500

    doco

--- lib/AnyEvent/FTP.pm
+++ lib/AnyEvent/FTP.pm
@@ -7,4 +7,46 @@ use v5.10;
 # ABSTRACT: Simple asynchronous ftp client and server
 # VERSION
 
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+L<Net::FTP>
+
+=item *
+
+L<Net::FTPServer>
+
+=item *
+
+L<AnyEvent>
+
+=item *
+
+L<RFC 959 FILE TRANSFER PROTOCOL|http://tools.ietf.org/html/rfc959>
+
+=item *
+
+L<RFC 2228 FTP Security Extensions|http://tools.ietf.org/html/rfc2228>
+
+=item *
+
+L<RFC 2640 Internationalization of the File Transfer Protocol|http://tools.ietf.org/html/rfc2640>
+
+=item *
+
+L<RFC 2773 Encryption using KEA and SKIPJACK|http://tools.ietf.org/html/rfc2773>
+
+=item *
+
+L<RFC 3659 Extensions to FTP|http://tools.ietf.org/html/rfc3659>
+
+=item *
+
+L<RFC 5797 FTP Command and Extension Registry|http://tools.ietf.org/html/rfc5797>
+
+=cut
+
 1;
